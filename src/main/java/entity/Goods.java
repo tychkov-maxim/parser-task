740e5e93341d7797a9dfd295e7f0709c21af6a11
Tychkov Maxim <tichkov_maxim@mail.ru>
2016-11-09 14:44:35 +0200

    add xml and xsd schemes

--- src/main/java/entity/Goods.java
+++ src/main/java/entity/Goods.java
@@ -4,6 +4,6 @@ public class Goods extends BaseEntity{
     private String name;
     private String description;
     private int price;//FIXME use joda money
-    private int groupId;
+    private int groupId;//FIXME need to make class or something else woth group or type
 
 }
